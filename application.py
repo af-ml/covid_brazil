488b9982466c4e1dc647f5d4abca47b7ce20a8fd
Private <private@AndreasBook2018.fritz.box>
2020-03-26 21:00:21 +0100

    prepare for eb

--- application.py
+++ application.py
@@ -10,52 +10,56 @@ import time
 
 development = go.Figure(data=[go.Scatter(x=[1, 2, 3, 4], y=[0, 1, 20, 200])])
 
-for i in range(5):
-    try:
-        r = requests.get('https://thevirustracker.com/free-api?countryTotal=BR').json()
-    except:
+def fetch_data():
+    r = requests.get("https://thevirustracker.com/free-api?countryTotal=BR", headers={"User-Agent": "XY"})
+    r = r.json()
 
+    country_data = r.get('countrydata',[])
+    country_data = country_data[0]
+    del country_data['info']
+    country_data = pd.DataFrame([country_data.values()],columns=country_data.keys())
+    return country_data
+#country_news = r.get('countrynewsitems',["No news"])[0]
+#country_news = pd.DataFrame(country_news).transpose()
 
-        print("request failed")
-        time.sleep(5)
-country_data = r.get('countrydata',[])
-country_data = country_data[0]
-del country_data['info']
-country_data = pd.DataFrame([country_data.values()],columns=country_data.keys())
-country_news = r.get('countrynewsitems',[])[0]
-country_news = pd.DataFrame(country_news).transpose()
-
-news_feed = []
-md = []
-for title,link in country_news[['title','url']].values:
-    md.append("[{}]({})".format(title,link))
-news_feed = '\n\n'.join(md)
+#news_feed = []
+#md = []
+#for title,link in country_news[['title','url']].values:
+#    md.append("[{}]({})".format(title,link))
+#news_feed = '\n\n'.join(md)
 
 
 external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 external_stylesheets = ["assets/dark.css"]
 
-application = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-application.layout = html.Div(
+#news_feed = []
+application = app.server
+
+update_seconds = 30
+app.layout = html.Div(
     [
         html.Div(
             className="app-header",
             children=[
                 html.H1("Estatísticas coronavírus no Brasil"),
+                dcc.Interval('graph-update',interval = update_seconds * 1000, n_intervals = 0),
                 dash_table.DataTable(
                     id="country_data",
-                    columns=[{"name": i, "id": i} for i in country_data.columns],
-                    data=country_data.to_dict("records"),
+                    columns=[{"name": i, "id": i} for i in fetch_data().columns],
+                    data=fetch_data().to_dict("records"),
                 ),
                 dcc.Graph(id="development", figure=development),
                 html.H2("notícias"),
-                dcc.Markdown(news_feed),
+                #dcc.Markdown(news_feed),
             ],
         )
     ]
 )
 
 if __name__ == "__main__":
-    application.debug=True
-    application.run_server()
+    application.run(debug=False, port=8080)
+
+
+
